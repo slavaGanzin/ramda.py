abf86bc26fd692342cb38a2dad28b0d7f09736ab
mohamedrafi <fair7993@gmail.com>
2018-03-02 06:43:45 +0530

    value in update_test changed

--- pyramda/iterable/update_test.py
+++ pyramda/iterable/update_test.py
@@ -3,8 +3,8 @@ from pyramda.private.asserts import assert_iterables_equal
 
 
 def cons_nocurry_test():
-    assert_iterables_equal(update(1, 1, [2, 3]), [2, 4])
+    assert_iterables_equal(update(1, 1, [2, 3]), [2, 1])
 
 
 def cons_curry_test():
-    assert_iterables_equal(update(1, 1)([2, 3]), [2, 4])
+    assert_iterables_equal(update(1, 1)([2, 3]), [2, 1])
