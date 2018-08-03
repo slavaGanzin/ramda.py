646982e9ee5baaf361b68c7f6fb764e07ebe96c2
Slava Ganzin <slava.ganzin@gmail.com>
2018-08-03 17:02:25 +0300

    cut nAry template strings

--- ramda/nAry.py
+++ ramda/nAry.py
@@ -6,9 +6,9 @@ def generate_args(spec, n):
     args1 = spec.args
     n_original = len(spec.args)
     add_args = ['None'] * (len(args1) - n)
-    args1.extend([f'x{i}' for i in range(0, n - len(args1))])
+    args1.extend(['x' + str(i) for i in range(0, n - len(args1))])
     args2 = args1[0:min(n, n_original)] + add_args
-    return args1[0:n], args2
+    return ", ".join(args1[0:n]), ", ".join(args2)
 
 
 @curry
@@ -20,5 +20,4 @@ def nAry(n, f):
 
     args1, args2 = generate_args(inspect.getfullargspec(f), n)
 
-    func = f'lambda {", ".join(args1)}: f({", ".join(args2)})'
-    return eval(func, {"f": f})
+    return eval('lambda ' + args1 + ': f(' + args2 + ')', {"f": f})
