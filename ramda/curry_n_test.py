974a25ab25037ff29fd9d86c855ac60c26220448
Slava Ganzin <slava.ganzin@gmail.com>
2018-08-09 16:40:02 +0300

    fix curry_n test

--- ramda/curry_n_test.py
+++ ramda/curry_n_test.py
@@ -6,15 +6,15 @@ def f(x, y, z=3):
     return x + y + z
 
 
-def run_f_curry_cases(fc):
+def f4_test():
     f4 = curry_n(4, f)
     assert_equal(f4(1, 1)(1, 1), 3)
+
+
+def f2_test():
     f2 = curry_n(2, f)
     try:
         f2(1, 2)
-        assert False, 'Should raise'
+        assert_equal(False, 'Should raise')
     except TypeError:
         pass
-
-    # assert_equal(fc(1, 1), 5)
-    # assert_equal(fc(1)(1), 5)
