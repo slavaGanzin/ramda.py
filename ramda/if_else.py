320524baa98410b332117fb070d44ca1b0d42b97
Slava Ganzin <slava.ganzin@gmail.com>
2018-08-14 14:13:34 +0300

    clarify if_else

--- ramda/if_else.py
+++ ramda/if_else.py
@@ -2,5 +2,5 @@ from ramda.curry import curry
 
 
 @curry
-def if_else(p, then_func, else_func, v):
-    return then_func(v) if p(v) else else_func(v)
+def if_else(predicate, on_true_func, on_false_func, value):
+    return on_true_func(value) if predicate(value) else on_false_func(value)
