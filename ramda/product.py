17295f524737066ed4945bb66a845e5a44064fb5
Slava Ganzin <slava.ganzin@gmail.com>
2018-09-12 11:59:40 +0300

    rewrite product

--- ramda/product.py
+++ ramda/product.py
@@ -1,6 +1,5 @@
-from ramda.curry import curry
-from ramda.reduce import reduce
-from .multiply import multiply
-
-
-product = reduce(multiply, 1)
+def product(xs):
+    result = xs[0]
+    for x in xs[1:]:
+        result *= x
+    return result
