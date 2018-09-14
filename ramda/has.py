b207be4eb2b5c8d6761bc907f20be22176a6f1fc
Slava Ganzin <slava.ganzin@gmail.com>
2018-09-14 16:44:47 +0300

    reimplement has

--- ramda/has.py
+++ ramda/has.py
@@ -4,11 +4,11 @@ from ramda.curry import curry
 @curry
 def has(name, o):
     """Returns whether or not an object has an own property with the specified name"""
+    if hasattr(o, name):
+        return True
+
     try:
         o[name]
         return True
-    except KeyError:
-        try:
-            return hasattr(o, name)
-        except AttributeError:
-            return False
+    except (TypeError, KeyError):
+        return False
--- ramda/has_test.py
+++ ramda/has_test.py
@@ -2,15 +2,21 @@ from ramda.has import has
 from ramda.private.asserts import *
 
 
-def has_curry_test():
+def has_test():
     has_name = has('name')
     assert_equal(has_name({'name': 'alice'}), True)
     assert_equal(has_name({'name': 'bob'}), True)
     assert_equal(has_name({}), False)
 
-
-def has_nocurry_test():
     point = {'x': 0, 'y': 0}
     assert_equal(has('x', point), True)
     assert_equal(has('y', point), True)
     assert_equal(has('z', point), False)
+
+    class X:
+        def __init__(self):
+            self.x = 1
+
+    x = X()
+    assert_equal(has('z', x), False)
+    assert_equal(has('x', x), True)
