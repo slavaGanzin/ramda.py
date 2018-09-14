86d645c0b403e206475d0f6ed5a42254e99188cc
Slava Ganzin <slava.ganzin@gmail.com>
2018-09-14 16:35:42 +0300

    props

---
+++ ramda/props.py
@@ -0,0 +1,9 @@
+from ramda.curry import curry
+from ramda.prop import prop
+
+
+@curry
+def props(properties, object):
+    """Acts as multiple prop: array of keys in, array of values out. Preserves
+order"""
+    return [prop(p, object) for p in properties]
---
+++ ramda/props_test.py
@@ -0,0 +1,14 @@
+from ramda import *
+from ramda.private.asserts import *
+
+
+def props_test():
+    assert_equal(props(['x', 'y'], {'x': 1, 'y': 2}), [1, 2])
+    assert_equal(props(['c', 'a', 'b'], {'b': 2, 'a': 1}), [None, 1, 2])
+
+    full_name = compose(join(' '), props(['first', 'last']))
+    assert_equal(full_name({
+        'last': 'Bullet-Tooth',
+        'age': 33,
+        'first': 'Tony'}),
+        'Tony Bullet-Tooth')
