33b220d7829b7a2f67a72c481de867457df79d79
Slava Ganzin <slava.ganzin@gmail.com>
2018-09-19 11:40:42 +0300

    when

---
+++ ramda/when.py
@@ -0,0 +1,13 @@
+from ramda.curry import curry
+
+
+@curry
+def when(predicate, when_true_fn, value):
+    """Tests the final argument by passing it to the given predicate function. If
+the predicate is satisfied, the function will return the result of calling
+the whenTrueFn function with the same argument. If the predicate is not
+satisfied, the argument is returned as is"""
+    if predicate(value):
+        return when_true_fn(value)
+
+    return value
---
+++ ramda/when_test.py
@@ -0,0 +1,13 @@
+from ramda import *
+from ramda.private.asserts import *
+
+
+truncate = when(
+    compose(gt(10), length),
+    pipe(take(10), append('…'), join(''))
+)
+
+
+def when_test():
+    assert_equal(truncate('12345'), '12345')
+    assert_equal(truncate('0123456789ABC'), '0123456789…')
