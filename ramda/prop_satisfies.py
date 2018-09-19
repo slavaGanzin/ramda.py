3488b68541a7a7a3f41686ad382ca493359c8b8f
Slava Ganzin <slava.ganzin@gmail.com>
2018-09-19 11:40:27 +0300

    prop_satisfies

---
+++ ramda/prop_satisfies.py
@@ -0,0 +1,10 @@
+from ramda.curry import curry
+from ramda.prop import prop
+
+
+@curry
+def prop_satisfies(predicate, property, object):
+    """Returns true if the specified object property satisfies the given
+predicate; false otherwise. You can test multiple properties with
+R.where"""
+    return predicate(prop(property, object))
---
+++ ramda/prop_satisfies_test.py
@@ -0,0 +1,8 @@
+from ramda import *
+from ramda.private.asserts import *
+
+
+def prop_satisfies_test():
+    assert_equal(
+        prop_satisfies(lt(2), 'x', {'x': 1, 'y': 2}),
+        True)
