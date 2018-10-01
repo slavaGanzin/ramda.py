8201892f48e70ac1214b2d0f885bdc5411f36674
Slava Ganzin <slava.ganzin@gmail.com>
2018-10-01 16:44:45 +0300

    take_last_while

---
+++ ramda/dissoc.py
@@ -0,0 +1,10 @@
+from ramda.curry import curry
+from ramda.clone import clone
+
+
+@curry
+def dissoc(key, object):
+    """Returns a new object that does not contain a prop property"""
+    o = clone(object)
+    del o[key]
+    return o
