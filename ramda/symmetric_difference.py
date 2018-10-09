f11cf8bc547b5b2bc9ccc2c4b127c0111486745a
Slava Ganzin <slava.ganzin@gmail.com>
2018-10-09 13:19:28 +0300

    symmetric_difference

---
+++ ramda/symmetric_difference.py
@@ -0,0 +1,13 @@
+from ramda.curry import curry
+from ramda.difference import difference
+from ramda.concat import concat
+
+
+@curry
+def symmetric_difference(first, second):
+    """Finds the set (i.e. no duplicates) of all elements contained in the first or
+second list, but not both"""
+    return concat(
+        difference(first, second),
+        difference(second, first)
+    )
---
+++ ramda/symmetric_difference_test.py
@@ -0,0 +1,11 @@
+from ramda import *
+from ramda.private.asserts import *
+
+
+def symmetric_difference_test():
+    assert_equal(symmetric_difference(
+        [1, 2, 3, 4], [7, 6, 5, 4, 3]),
+        [1, 2, 7, 6, 5])
+    assert_equal(symmetric_difference(
+        [7, 6, 5, 4, 3], [1, 2, 3, 4]),
+        [7, 6, 5, 1, 2])
