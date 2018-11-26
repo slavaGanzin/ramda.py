3aa84039ba373850dc4477b8bede25f6054c1672
Slava Ganzin <slava.ganzin@gmail.com>
2018-11-26 13:03:07 +0200

    fix naming issues in tests

--- ramda/T.py
+++ ramda/T.py
@@ -1,5 +1,3 @@
 def T(*args):
-    """Creates a new list out of the two supplied by applying the function to each
-equally-positioned pair in the lists. The returned list is truncated to the
-length of the shorter of the two input lists"""
+    """A function that always returns true. Any passed in parameters are ignored"""
     return True
