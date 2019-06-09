52a4e66fe1841c9314aac208752a4c8c419f54dc
Slava Ganzin <slava.ganzin@gmail.com>
2019-06-09 16:20:26 +0300

    take _list(list)

--- ramda/take.py
+++ ramda/take.py
@@ -1,4 +1,5 @@
 from ramda.curry import curry
+from builtins import list as _list
 
 
 @curry
@@ -6,10 +7,7 @@ def take(n, list):
     """Returns the first n elements of the given list, string, or
 transducer/transformer (or object with a take method).
 Dispatches to the take method of the second argument, if present"""
-    a = []
-    i = 1
-    for x in list:
-        if i > n:
-            return a
-        a.append(x)
-        i += 1
+    try:
+        return list[:n]
+    except TypeError:
+        return _list(list)[:n]
