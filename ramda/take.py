f64bf17aa291db7447ed4ae71e6df1a28cc2a348
Slava Ganzin <slava.ganzin@gmail.com>
2019-06-09 15:33:52 +0300

    reimplement take with accumulator: compatible with x.items()

--- ramda/take.py
+++ ramda/take.py
@@ -6,4 +6,10 @@ def take(n, list):
     """Returns the first n elements of the given list, string, or
 transducer/transformer (or object with a take method).
 Dispatches to the take method of the second argument, if present"""
-    return list[:n]
+    a = []
+    i = 1
+    for x in list:
+        if i > n:
+            return a
+        a.append(x)
+        i += 1
