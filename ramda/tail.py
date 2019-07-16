7d4dfba843909f98b0aac7da282a3c9e5f04f2f1
Slava Ganzin <slava.ganzin@gmail.com>
2019-07-16 09:31:35 +0300

    init/tail add list wrapper

--- ramda/tail.py
+++ ramda/tail.py
@@ -2,4 +2,4 @@ def tail(xs):
     """Returns all but the first element of the given list or string (or object
 with a tail method).
 Dispatches to the slice method of the first argument, if present"""
-    return xs[1:]
+    return list(xs)[1:]
