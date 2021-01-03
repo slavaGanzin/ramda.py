2fd6bdc833dd6eef22af41d2d0450cd38219183b
Slava Ganzin <slava.ganzin@gmail.com>
2021-01-03 16:28:52 +0200

    has: hasattr force string attribute name

--- ramda/has.py
+++ ramda/has.py
@@ -4,7 +4,7 @@ from ramda.curry import curry
 @curry
 def has(name, o):
     """Returns whether or not an object has an own property with the specified name"""
-    if hasattr(o, name):
+    if hasattr(o, str(name)):
         return True
 
     try:
