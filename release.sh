6c2746f8d349bd318ba52e890fdc73d6834e715b
Slava Ganzin <slava.ganzin@gmail.com>
2021-07-21 21:58:14 +0300

    publish with twine

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 rm -rf dist build ramad.egg-info
 python3 setup.py sdist bdist_wheel
-python3 -m twine  upload  dist/* --skip-existing -u slavaGanzin
+twine  upload  dist/* --skip-existing -u slavaGanzin
 sudo pip3 install --upgrade --ignore-installed ramda
