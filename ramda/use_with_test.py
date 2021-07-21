68a3c1c985997d759a2ef786153fc5b87c94d8ba
Jakob J. Kolb <kolb@hardfork.io>
2021-07-21 18:25:11 +0200

    Enable lambda functions as transformers in use_with.

--- ramda/use_with_test.py
+++ ramda/use_with_test.py
@@ -7,3 +7,5 @@ def use_with_test():
     assert_equal(use_with(pow, [identity, identity])(3)(4), 81)
     assert_equal(use_with(pow, [dec, inc])(3, 4), 32)
     assert_equal(use_with(pow, [dec, inc])(3)(4), 32)
+
+    assert_equal(use_with(pow, [lambda x: x-1, inc])(3, 4), 32)
