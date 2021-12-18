fd0594655cad3e7591bcefaaec422844a9ca48bc
jakobkolb <jakob.j.kolb@gmail.com>
2021-12-18 23:57:46 +0100

    Fix apply_spec instance to return different result when used more than once.

--- ramda/apply_spec.py
+++ ramda/apply_spec.py
@@ -7,9 +7,10 @@ def apply_spec(spec):
     """Given a spec object recursively mapping properties to functions, creates a
     function producing an object of the same structure, by mapping each property
     to the result of calling its associated function with the supplied arguments"""
-    out = clone(spec)
+
 
     def spec_applier(*args):
+        out = clone(spec)
         for k, v in spec.items():
             try:
                 v.items()
--- ramda/apply_spec_test.py
+++ ramda/apply_spec_test.py
@@ -7,3 +7,13 @@ get_metrics = apply_spec({"sum": add, "nested": {"mul": multiply}})
 
 def apply_spec_test():
     assert_equal(get_metrics(2, 4), {"sum": 6, "nested": {"mul": 8}})
+
+    res = map(
+        apply_spec(
+            {
+                "foo": prop("bar"),
+            }
+        ),
+        [{"bar": 1}, {"bar": 2}],
+    )
+    assert res == [{"foo": 1}, {"foo": 2}]
