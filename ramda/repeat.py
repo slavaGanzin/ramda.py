378705190a11c03d61ce6f9dc9f495bc9742d430
Slava Ganzin <slava.ganzin@gmail.com>
2021-12-20 19:17:39 +0200

    curry repeat fix #21

--- ramda/repeat.py
+++ ramda/repeat.py
@@ -2,6 +2,7 @@ from toolz import curry
 from ramda import clone
 
 
+@curry
 def repeat(value, times):
     """Returns a fixed list of size n containing a specified identical value"""
     return [clone(value) for i in range(0, times)]
--- ramda/times.py
+++ ramda/times.py
@@ -1,5 +1,4 @@
-"""This functions returns list from 1 to n"""
-
-
+@curry
 def times(function, n):
+    """Calls an input function n times, returning an array containing the results of those function calls. fn is passed one argument: The current value of n, which begins at 0 and is gradually incremented to n - 1."""
     return [function(i) for i in range(0, n)]
