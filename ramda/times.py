1a53634201f16f9b553ba6ea86a34615ae82d0ab
Slava Ganzin <slava.ganzin@gmail.com>
2021-12-20 19:18:54 +0200

    fix curry repeat fix #21

--- ramda/times.py
+++ ramda/times.py
@@ -1,3 +1,6 @@
+from toolz import curry
+
+
 @curry
 def times(function, n):
     """Calls an input function n times, returning an array containing the results of those function calls. fn is passed one argument: The current value of n, which begins at 0 and is gradually incremented to n - 1."""
--- ramda/times_test.py
+++ ramda/times_test.py
@@ -5,4 +5,4 @@ from ramda.private.asserts import assert_iterables_equal
 
 def uniq_nocurry_test():
     assert_iterables_equal(times(identity, 5), [0, 1, 2, 3, 4])
-    assert_iterables_equal(times(identity)( 5s), [0, 1, 2, 3, 4])
+    assert_iterables_equal(times(identity)(5), [0, 1, 2, 3, 4])
