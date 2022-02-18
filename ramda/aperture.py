e55ea281b035f7fef2ee84559714e759eaefceaa
Slava Ganzin <slava.ganzin@gmail.com>
2022-02-18 18:58:20 +0200

    mnieber's aperture fix #30

--- ramda/aperture.py
+++ ramda/aperture.py
@@ -6,6 +6,4 @@ def aperture(n, xs):
     """Returns a new list, composed of n-tuples of consecutive elements. If n is
     greater than the length of the list, an empty list is returned.
     Acts as a transducer if a transformer is given in list position"""
-    return [
-        list(xs)[i : i + n] for i in range(0, int(len(xs) / n) * n) if i + 1 != len(xs)
-    ]
+    return [list(xs)[i : i + n] for i in range(0, len(xs) - n + 1)]
