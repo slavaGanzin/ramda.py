524eecb790e5f88cd89103e9f500f0b742b646e6
Slava Ganzin <slava.ganzin@gmail.com>
2022-02-24 18:50:39 +0200

    improve test_uniq

--- test/test_uniq_by.py
+++ test/test_uniq_by.py
@@ -4,3 +4,12 @@ from ramda.private.asserts import *
 
 def test_uniq_by():
     assert_equal(uniq_by(abs, [-1, -5, 2, 10, 1, 2]), [-1, -5, 2, 10])
+    assert_equal(
+        uniq_by(abs, {-1, -5, 2, 10, 1, 2}),
+        [
+            1,
+            2,
+            10,
+            -5,
+        ],
+    )
